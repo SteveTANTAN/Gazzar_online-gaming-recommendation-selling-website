bd100e061cb3ef13e4fcbf69cf42f9476b02f7b3
fosterchen <z5237481@ad.unsw.edu.au>
2022-03-20 00:49:15 +1100

    finish the edit_product and get_product

--- backend/admin/src/product_manage.py
+++ backend/admin/src/product_manage.py
@@ -17,6 +17,7 @@ from help_admin import ErrorMessage, create_product_id, token_to_id, add_to_data
 
 def add_product(token, input_product_dict, product_category):
     product_dict = json.loads(input_product_dict)
+
     # handle token valid
     cur_admin_id = token_to_id(token)
     admins = Admin.query.filter((Admin.admin_id==cur_admin_id)).all()
@@ -37,7 +38,7 @@ def add_product(token, input_product_dict, product_category):
     product_id = create_product_id()
     new_product = Product(product_id, product_name, product_description,
                     product_price, product_discount, product_status, product_stock, product_main_image, product_sub_image, product_category)
-
+    new_product.last_modified = admins[0].email
     # add product type
     for product_type_name in product_dict['Product Type']:
         new_product_type = Type.query.filter(Type.type_name == product_type_name).all()[0]
@@ -46,32 +47,63 @@ def add_product(token, input_product_dict, product_category):
 
     return {'is_success': True}
 
-# def get_product(token, product_id):
-#     # handle token valid
-#     cur_admin_id = token_to_id(token)
-#     admins = Admin.query.filter((Admin.admin_id==cur_admin_id)).all()
-#     if (len(admins) == 0):
-#         raise ErrorMessage(Error.query.filter(Error.error_id == 2).all()[0].error_name)
-#     # get product
-#     output = {}
-#     target_product = Product.query.filter(Product.product_id == product_id).all()[0]
-#     output['product_name'] = target_product.product_name
-#     # output['product_']
-#     return output
+def get_product(token, product_id):
+    # handle token valid
+    cur_admin_id = token_to_id(token)
+    admins = Admin.query.filter((Admin.admin_id==cur_admin_id)).all()
+    if (len(admins) == 0):
+        raise ErrorMessage(Error.query.filter(Error.error_id == 2).all()[0].error_name)
 
-# def edit_product(token, product_dict):
-#     # handle token valid
-#     cur_admin_id = token_to_id(token)
-#     admins = Admin.query.filter((Admin.admin_id==cur_admin_id)).all()
-#     if (len(admins) == 0):
-#         raise ErrorMessage(Error.query.filter(Error.error_id == 2).all()[0].error_name)
+    # get product
+    output = {}
+    target_product = Product.query.filter(Product.product_id == product_id).all()[0]
+    output['Product Name'] = target_product.name
+    output['Product description'] = target_product.description
+    output['State'] = target_product.status
+    output['Stock'] = target_product.stock
+    output['Unit Price'] = target_product.price
+    output['Discount'] = target_product.discount
+    output['Cover'] = ast.literal_eval(target_product.main_image)
+    #print(type(output['Cover']))
+    output['Photo'] = ast.literal_eval(target_product.sub_image)
+    output_type = []
+    for product_type_name in target_product.genre:
+        output_type.append(product_type_name.type_name)
+    output['Product Type'] = output_type
+    #print(output)
+    return output
 
-#     return {'is_success': True}
+def edit_product(token, input_product_dict):
+    product_dict = json.loads(input_product_dict)
+    # handle token valid
+    cur_admin_id = token_to_id(token)
+    admins = Admin.query.filter((Admin.admin_id==cur_admin_id)).all()
+    if (len(admins) == 0):
+        raise ErrorMessage(Error.query.filter(Error.error_id == 2).all()[0].error_name)
+
+    # add product
+    target_product = Product.query.filter(Product.product_id == product_dict['Product Id']).all()[0]
+    target_product.name = product_dict['Product Name']
+    target_product.description = product_dict['Product description']
+    target_product.status = int(product_dict['State'])
+    target_product.stock = product_dict['Stock']
+    target_product.price = product_dict['Unit Price']
+    target_product.discount = product_dict['Discount']
+
+    target_product.main_image = str(product_dict['Cover'])
+    target_product.sub_image = str(product_dict['Photo'])
+    target_product.last_modified = admins[0].email
+
+    target_product.genre = []
+    for product_type_name in product_dict['Product Type']:
+        new_product_type = Type.query.filter(Type.type_name == product_type_name).all()[0]
+        target_product.genre.append(new_product_type)
+        db.session.commit()
+
+    #print(target_product.genre)
+    return {'is_success': True}
 
 # if __name__ == "__main__":
 #     # show_product()
-#     list_a = "[{'uid': 'rc-upload-1647614711752-3', 'lastModified': 1647614740987},{'uid': 'rc-upload-1647614711752-3', 'lastModified': 1647614740987}]"
-#     b = ast.literal_eval(list_a)
-#     print(type(list_a))
-#     print(b[0]['uid'])
+#     get_product("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhZG1pbl9pZCI6NH0.8AuOI2nvMER0IQug3_H-Quz1WEjeDWRhccsh-A61rYo", 1)
     
