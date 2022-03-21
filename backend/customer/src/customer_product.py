eff9472442a2cc0673ea7326d4fe3fc037733922
Jeffery-Lee-QAQ <z5237312@ad.unsw.edu.au>
2022-03-21 12:13:48 +0800

    add buy_now function

--- backend/customer/src/customer_product.py
+++ backend/customer/src/customer_product.py
@@ -29,7 +29,6 @@ def search(str):
     for i in search_result:
         temp = {"product_id": i.product_id, "name": i.name}
         result.append(temp)
-    #result =[{"product_id": 31, "name": "Grand Theft Auto (series)"}, {"product_id": 32, "name": "Grand Theft Auto (series)"}, {"product_id": 33, "name": "Grand Theft Auto (series)"}, {"product_id": 34, "name": "Grand Theft Auto (series) on PC"}, {"product_id": 35, "name": "Grand Theft Auto (series)"}, {"product_id": 36, "name": "Grand Theft Auto (series)"}, {"product_id": 37, "name": "Grand Theft Auto III"}, {"product_id": 38, "name": "Grand Theft Auto V"}, {"product_id": 39, "name": "Grand Theft Auto: Episodes from Liberty City"}, {"product_id": 40, "name": "Grand Theft Auto: Vice City"}, {"product_id": 43, "name": "Hearts of Iron"}]
     return result
 
 def show_product_details(token, product_id):
@@ -46,7 +45,6 @@ def show_product_details(token, product_id):
 
     # convert main_image and sub_image string to list
     cover = ast.literal_eval(target_product.main_image)
-    cover_image = [{'image': cover[0]['thumbUrl']}]
     photo = ast.literal_eval(target_product.sub_image)
     photo_images = []
     for i in photo:
@@ -60,16 +58,42 @@ def show_product_details(token, product_id):
 
     product_details = {'product_id': target_product.product_id, 'name': target_product.name, 'description': target_product.description,
                     'price': float(target_product.price), 'discount': target_product.discount, 'stock': target_product.stock,
-                    'main_image': cover_image, 'sub_image': photo_images, 'rate': float(target_product.rate), 'comment': target_product.comment, 'type': tags}
+                    'main_image': cover[0]['thumbUrl'], 'sub_image': photo_images, 'rate': float(target_product.rate), 'comment': target_product.comment, 'type': tags}
 
     return product_details
 
-if __name__ == "__main__":
-    db.create_all()
-    result = show_product_details('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOjB9.iQfXIXBl6UUzeise2YrpHK43XimDKNSu6iCE7NKtB5w', 123)
-    # result = search('grand')
-    pprint.pprint(result)
+def buy_now(token, product_id, quantity):
+    u_id = token_to_id(token)
+    # check valid user
+    users = User.query.filter(User.user_id==u_id).all()
+    if len(users) == 0:
+        raise ErrorMessage(Error.query.filter(Error.error_id == 17).all()[0].error_name)
+
+    target_product = Product.query.filter(Product.product_id==product_id, Product.user_id==u_id).all()[0]
+    cover = ast.literal_eval(target_product.main_image)
+    target_product_info = {
+        'product_id': target_product.product_id,
+        'name': target_product.name,
+        'description': target_product.description,
+        'main_image': cover[0]['thumbUrl'],
+        'current_price': float(target_product.price) * float(target_product.discount) * (0.01),
+        'quantity': quantity,
+    }
+    original_price = float(target_product.price * quantity)
+    total_discount = float((100 - target_product.discount) * (0.01) * original_price)
+    actual_transaction = float(original_price - total_discount)
+    output = {
+        'checkout_product': target_product_info,
+        'original_price': original_price,
+        'total_discount': total_discount,
+        'actual_transaction': actual_transaction
+    }
+    return output
 
 
 
- #cart input pro_id pro_quantity token
+# if __name__ == "__main__":
+#     db.create_all()
+#     result = show_product_details('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOjB9.iQfXIXBl6UUzeise2YrpHK43XimDKNSu6iCE7NKtB5w', 20)
+#     # result = search('grand')
+#     pprint.pprint(result)
